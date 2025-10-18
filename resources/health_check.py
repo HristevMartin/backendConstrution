47acf8f5d42e61578c761faf4c3af110e25781a9
marto9696 <m.hristev@ubiquedigital.com>
2025-10-18 13:13:49 +0100

    update

--- resources/health_check.py
+++ resources/health_check.py
@@ -10,4 +10,4 @@ class HealthCheck(Resource):
 
 class SecondHealthCheck(Resource):
     def get(self):
-        return {'message': 'Second health checkss successsful'}, 200
+        return {'message': 'Second healtsh checkss successsful'}, 200
