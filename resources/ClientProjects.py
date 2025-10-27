b90f52f3128e71577fb541826d24d25ba4990e4a
marto9696 <m.hristev@ubiquedigital.com>
2025-10-27 13:49:16 +0000

    update

--- resources/ClientProjects.py
+++ resources/ClientProjects.py
@@ -89,7 +89,7 @@ class ClientProjects(Resource):
             
             print(f"Using userId: {user_id} for bucket organization")
             
-            # Handle multiple file uploads using Google Cloud Storage (optional)
+            # Handle multiple file usploads using Google Cloud Storage (optional)
             project_images = request.files.getlist('images')
             image_urls = []
             
